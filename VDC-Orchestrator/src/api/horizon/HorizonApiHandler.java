de96fd9937c84f61249f02df5172425d243aa052
daniel_sanchez <daniel_sanchez@bitbucket.i2cat.net>
2016-05-05 10:48:42 +0200

    made some changes

--- VDC-Orchestrator/src/api/horizon/HorizonApiHandler.java
+++ VDC-Orchestrator/src/api/horizon/HorizonApiHandler.java
@@ -66,53 +66,59 @@ public class HorizonApiHandler implements HttpHandler{
 					if(db.existVDC(vdc.getTenant())){
 						ErrorCheck res = hac.deployTopology(Conf.IP_Heat, vdc.getTenant(), token, "PUT");
 						if(res == ErrorCheck.ALL_OK)
-							db.deleteVDC(vdc.getTenant());
+							resRequest(db.deleteVDC(vdc.getTenant()), e, "DELETE", "");
 						else
 							resRequest(res, e, "POST", "");
 					}
-					ErrorCheck ec = dm.startParse(vdc);
-					resRequest(ec,e,"POST","");
-					
-					/*List<Tenant> tenants = kac.getTenant(Conf.IP_Keystone, token);
-					String id = "";
-					for(Tenant aux : tenants){
-						if(aux.getName().equals("admin"))
-							id = aux.getId();
-					}
-					
-					ArrayList<Flavor> flavors = nac.getFlavors(Conf.IP_Nova, token, parser, id);
-					
-					System.out.println(flavors.toString());
-					
-					Map<String,Host> hosts = nac.getHosts(Conf.IP_Nova, token, parser, id);
-					
-					dbnova.startDB();
-					ResultSet rs = dbnova.queryDB();
-					Host aux;
-					SSHclient ssh = new SSHclient();
-					
-					while(rs.next()){
-						if(hosts.containsKey(rs.getString("host"))){
-							aux = hosts.get(rs.getString("host"));
-							ssh.connect(Conf.User_Compute,rs.getString("host_ip"),22,Conf.Pass_Compute);					
-							String[] output = ssh.ExecuteIfconfig();
-							Map<String,String> mac = checkMac(output, aux);
-							for(Entry<String, String> set : mac.entrySet()){
-								if(!set.getKey().equals("127.0.0.1") && !set.getKey().equals(rs.getString("host_ip")))
-										aux.setMac(set.getValue());
-							}
+					else{
+						ErrorCheck ec = dm.startParse(vdc);
+						if(!ec.equals(ErrorCheck.ALL_OK))
+							resRequest(ec,e,"POST","");
+						
+						/*List<Tenant> tenants = kac.getTenant(Conf.IP_Keystone, token);
+						String id = "";
+						for(Tenant aux : tenants){
+							if(aux.getName().equals("admin"))
+								id = aux.getId();
 						}
-						ssh.disconnect();
+						
+						ArrayList<Flavor> flavors = nac.getFlavors(Conf.IP_Nova, token, parser, id);
+						
+						System.out.println(flavors.toString());
+						
+						Map<String,Host> hosts = nac.getHosts(Conf.IP_Nova, token, parser, id);
+						
+						dbnova.startDB();
+						ResultSet rs = dbnova.queryDB();
+						Host aux;
+						SSHclient ssh = new SSHclient();
+						
+						while(rs.next()){
+							if(hosts.containsKey(rs.getString("host"))){
+								aux = hosts.get(rs.getString("host"));
+								ssh.connect(Conf.User_Compute,rs.getString("host_ip"),22,Conf.Pass_Compute);					
+								String[] output = ssh.ExecuteIfconfig();
+								Map<String,String> mac = checkMac(output, aux);
+								for(Entry<String, String> set : mac.entrySet()){
+									if(!set.getKey().equals("127.0.0.1") && !set.getKey().equals(rs.getString("host_ip")))
+											aux.setMac(set.getValue());
+								}
+							}
+							ssh.disconnect();
+						}
+						for(Entry<String, Host> host : hosts.entrySet()){
+							  System.out.println("Host name=" + host.getKey() + ", Host_info " + host.getValue().toString());
+						}
+						
+						Topology topology = new Topology();
+						OdlApiClient odlApi = new OdlApiClient();
+						odlApi.getResources(topology,hosts);
+						
+						dbnova.stopDB();*/
+						
+						ErrorCheck res = hac.deployTopology(Conf.IP_Heat, vdc.getTenant(), token, "POST");
+						resRequest(res, e, "POST", "");
 					}
-					for(Entry<String, Host> host : hosts.entrySet()){
-						  System.out.println("Host name=" + host.getKey() + ", Host_info " + host.getValue().toString());
-					}
-					
-					Topology topology = new Topology();
-					OdlApiClient odlApi = new OdlApiClient();
-					odlApi.getResources(topology,hosts);
-					
-					dbnova.stopDB();*/
 				}
 				else{
 					resRequest(ErrorCheck.BAD_CONTENT_TYPE,e,"POST","");
@@ -129,16 +135,11 @@ public class HorizonApiHandler implements HttpHandler{
 				String tenant = checkQuery(query);
 				if(!tenant.isEmpty()){
 					if(db.existVDC(tenant)){
-						/*String token = kac.getToken(Conf.IP_Keystone, Conf.User_Keystone,Conf. Pass_Keystone, "default");
+						String token = kac.getToken(Conf.IP_Keystone, Conf.User_Keystone,Conf. Pass_Keystone, "default");
 						System.out.println(token);
 						ErrorCheck res = hac.deployTopology(Conf.IP_Heat, tenant, token, "DELETE");
-						if(res == ErrorCheck.ALL_OK){
-							//TODO Eliminar stack de bd y local*/
-							ErrorCheck ec = db.deleteVDC(vdc.getTenant());
-							resRequest(ec,e,"DELETE","");
-						/*}
-						//ErrorCheck ec = db.showDB(new VDC(), "vdc", tenant, "delete");
-						*/
+						if(res == ErrorCheck.ALL_OK)
+							resRequest(db.deleteVDC(vdc.getTenant()), e, "DELETE", "");
 					}
 					else{
 						resRequest(ErrorCheck.TENANTID_NOT_FOUND,e,"DELETE","");
